7c34594afea8615f449e18cdcd1597d0e046b580
Fabricio Catae <fcatae@microsoft.com>
2017-03-06 00:30:21 -0300

    controller

---
+++ src/controller.ts
@@ -0,0 +1,25 @@
+// include 'proxy'
+// include 'viewmodel'
+// include 'ui'
+
+// setup
+//   viewmodel: create the folders
+
+// initial state
+// call proxy to get data (state)
+//   proxy: get_initial
+//   viewmodel: for each task: create task
+
+// update_state
+//   proxy: update_state
+//   viewmodel: update_state
+
+// update_data
+//   proxy: update_data
+//   viewmodel: update_data
+
+// ui.event: task dropped in folder
+//   update_state(task, folder src, folder dst)
+
+// ui.event: modify task
+//   update_data
---
+++ src/proxy.ts
@@ -0,0 +1,1 @@
+// call Kanban REST API
