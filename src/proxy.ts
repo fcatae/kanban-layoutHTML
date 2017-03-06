37246409005325c4fbdecefc8c07cca0a55c1e3e
Fabricio Catae <fcatae@microsoft.com>
2017-03-06 18:54:26 -0300

    initial design

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,2 +1,66 @@
 // abstract calls
 //  eg. call Kanban REST API
+
+class WorkspaceProxy {    
+
+    // GET workspaces/<wid>/folders/default
+    public getDefaultFolderId() : string {
+        return 'default';
+    }
+
+    // GET workspaces/<wid>/folders
+    public getFolderIds() : Array<string> {
+        return new Array<string>();
+    }
+
+    // GET workspaces/<wid>/folders/<fid>/tasks
+    public getTasks() {
+    }
+
+    // POST workspace/<wid>/folders/<fid>/tasks
+    public createTask(id, title, data) : TaskProxy {
+        return new TaskProxy();
+    }
+
+    // POST workspaces/<wid>/folders/<fid>/tasks/<tid>/move ? to=<fid>
+    public setTaskFolder(task, folder) {        
+    }
+
+    // GET workspaces/<wid>/folders/<fid>/tasks/<tid>
+    public getData(id) : string {        
+        return '';
+    }
+
+    // GET workspaces/<wid>/folders/<fid>/tasks/<tid>
+    public updateData(id, title, data) : TaskProxy {
+        return new TaskProxy();
+    }    
+}
+
+class WorkspaceFolderProxy {
+    workspaceId: string;
+    folderId: string;
+    taskFolderId: string;
+    name: string;
+}
+
+class TaskCollectionProxy {
+    id: string;
+    tasks: Array<TaskProxy>;
+    next: TaskCollectionProxy | null;
+}
+
+class TaskProxy {    
+    id: string;
+    title: string;
+    preview: string;
+}
+
+class TaggedTaskProxy {    
+    id: string;
+    title: string;
+    preview: string;
+    tag: string;
+    tag_data: string;
+} // technology , metric , etc
+
--- src/viewmodel.ts
+++ src/viewmodel.ts
@@ -2,3 +2,36 @@
 //  - angular
 //  - react
 //  - jquery
+
+class WorkspaceModel {    
+
+    public getDefaultWorkspaceFolder() {
+    }
+
+    public getFolders() {
+    }
+
+    public createTask(title, data) {        
+    }
+
+    public setTaskFolder(task, folder) {        
+    }
+
+}
+
+class FolderView {
+    id: string;
+    name: string;
+    tasks: Array<TaskView>;
+}
+
+class TaskView {    
+    id: string;
+    title: string;
+    data: string;
+
+    folder: FolderView;
+
+    public updateData(title, data) {        
+    }
+}
